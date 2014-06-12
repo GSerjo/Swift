8faad5780e95dcb289b53f3965055eb277b461dd
sedot8@gmail.com <Serjo@Serjos-MacBook-Pro.local>
2014-06-13 03:00:09 +0400

    * Test

--- Hello Tanako/Hello Tanako/AppDelegate.swift
+++ Hello Tanako/Hello Tanako/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
     @IBOutlet var btSay : NSButton
+    
     @IBOutlet var tbName : NSTextField
     @IBOutlet var lbHello : NSTextField
     
